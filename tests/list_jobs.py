ad3fb2b7ccc0cee3810e69d765bce4d07175c60d
Jeffrey Salmond <js947@users.noreply.github.com>
2020-08-19 20:52:47 +0000

    improve dataframe format

--- tests/list_jobs.py
+++ tests/list_jobs.py
@@ -2,5 +2,8 @@ from rescale import Rescale
 import pandas as pd
 
 rescale = Rescale()
-jobs = pd.DataFrame(rescale.multiget("jobs"))
+jobs = pd.DataFrame(
+    dict(id=j['id'], name=j['name'])
+    for j in rescale.multiget("jobs")
+)
 print(jobs)
