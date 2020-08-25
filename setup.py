239e991bd0722adff1b4697d2d87547b735a183d
Jeffrey Salmond <jeffrey@cambridgesupercomputing.com>
2020-08-25 16:15:05 +0000

    reformat with black

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+
 setup(
     name="Rescale API",
     version="0.1",
-    packages=['rescale'],
+    packages=["rescale"],
     scripts=[],
     install_requires=["requests", "keyring"],
     package_data={},
@@ -10,5 +11,5 @@ setup(
     author_email="jsalmond@rescale.com",
     description="Jeffrey's Rescale API widget",
     keywords="hello world example examples",
-    url="http://github.com/js947/rescale-api/",   # project home page, if any
+    url="http://github.com/js947/rescale-api/",  # project home page, if any
 )
